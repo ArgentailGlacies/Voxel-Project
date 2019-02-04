cf6ea29c55bc1f7fb704cf2e9fa3b12d34859cbe
Kristian Mikalsen <Kmikalsen@hotmail.com>
2019-02-04 17:39:49 +0100

    Added bitmap wrapper

---
+++ Core/src/allegro/Bitmap.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include "io/File.h"
+
+#include <glm/vec2.hpp>
+#include <glm/vec4.hpp>
+#include <memory>
+
+struct ALLEGRO_BITMAP;
+struct ALLEGRO_LOCKED_REGION;
+
+namespace core
+{
+	class Bitmap
+	{
+	public:
+		Bitmap() = default;
+		Bitmap(const Bitmap & other) { *this = other; }
+		Bitmap(Bitmap && other) { *this = std::move(other); }
+		~Bitmap() { clear(); }
+
+		Bitmap & operator=(const Bitmap & other);
+		Bitmap & operator=(Bitmap && other);
+
+		/**
+			Constructs a child bitmap from this bitmap. The child will be invalid once the parent
+			bitmap goes out of scope. It is the user's responsibility to ensure that the child
+			bitmap is not used in that situation.
+
+			@param pos The location of the child bitmap in the parent bitmap.
+			@param size The size of the child bitmap.
+			@return The child bitmap.
+		*/
+		Bitmap child(const glm::ivec2 & pos, const glm::ivec2 & size) const;
+
+		/**
+			Creates a new blank bitmap of the provided size. The bitmap should not be too large, as
+			the memory requirements are significant.
+
+			@param width The width of the bitmap to create.
+			@param height The height of the bitmap to create.
+		*/
+		void create(unsigned int width, unsigned int height);
+		/**
+			Loads a bitmap from the provided file. The bitmap may be of any file format accepted by
+			Allegro.
+
+			@param file The file containing the bitmap to load.
+		*/
+		void load(const util::File & file);
+		/**
+			Saves the bitmap to the provided file. The bitmap may be saved as any file format
+			supported by Allegro.
+
+			@param file The file to save the bitmap to.
+		*/
+		void save(const util::File& file);
+
+		/**
+			Erases any underlying bitmap handles, releasing any used memory.
+		*/
+		void clear();
+
+		// ...
+
+		/**
+			Determines how the user may interact with the bitmap after it has been locked.
+		*/
+		enum class LockMode
+		{
+			READWRITE = 0,
+			READONLY = 1,
+			WRITEONLY = 2
+		};
+
+		/**
+			A locked region can be used to read or write data from or to the underlying bitmap in an
+			efficient manner. The bitmap will automatically be unlocked once the locked region goes
+			out of scope.
+		*/
+		class LockedRegion
+		{
+		public:
+			LockedRegion(ALLEGRO_BITMAP * bitmap, ALLEGRO_LOCKED_REGION * region);
+			~LockedRegion();
+
+			/**
+				@return The raw data associated with the bitmap. The data is on a format expected by
+				OpenGL's textures and as such may be directly fed to it.
+			*/
+			const void * data() const;
+
+		private:
+			ALLEGRO_BITMAP * m_bitmap;
+			ALLEGRO_LOCKED_REGION * m_region;
+		};
+
+		/**
+			Locks the bitmap such that its data is immutable. The bitmap must not be used for any
+			drawing operations while it is locked!
+		*/
+		LockedRegion lock(LockMode mode);
+
+		// ...
+
+		/**
+			Renders the entire bitmap using the specified position, size, and tint.
+
+			@param pos The position the bitmap should be rendered at.
+			@param size The desired size the bitmap should be rendered to.
+			@param tint The color muliplier of the bitmap.
+		*/
+		void draw(const glm::vec2 & pos, const glm::vec2 & size, const glm::vec4 & tint) const;
+		/**
+			Renders part of the bitmap using the specified position, size, center, and tint.
+
+			@param source The position and size the bitmap should be rendered with.
+			@param target The desired target and size the bitmap should be rendered with.
+			@param tint The color muliplier of the bitmap.
+		*/
+		void draw(const glm::vec4 & source, const glm::vec2 & center, const glm::vec4 & target, const glm::vec4 & tint) const;
+
+		// ...
+
+		inline auto * handle() const { return m_handle; }
+
+	private:
+		ALLEGRO_BITMAP * m_handle = nullptr;
+	};
+}
---
+++ CoreTest/src/allegro/BitmapTest.cpp
@@ -0,0 +1,99 @@
+
+#include "allegro/Bitmap.h"
+
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace core::allegro
+{
+	TEST_CLASS(BitmapTest)
+	{
+	public:
+		~BitmapTest()
+		{
+			m_fileA.erase();
+			m_fileB.erase();
+		}
+
+		TEST_METHOD(Bitmap_ctor)
+		{
+			Bitmap temporary;
+			temporary.create(32, 32);
+
+			Bitmap bitmapA;
+			Bitmap bitmapB = temporary;
+			Bitmap bitmapC = std::move(temporary);
+
+			Assert::IsNull(bitmapA.handle());
+			Assert::IsNotNull(bitmapB.handle());
+			Assert::IsNotNull(bitmapC.handle());
+			Assert::IsFalse(bitmapB.handle() == bitmapC.handle());
+		}
+
+		TEST_METHOD(Bitmap_child)
+		{
+			Bitmap parent, child;
+			parent.create(32, 32);
+			child = parent.child({ 8, 8 }, { 12, 16 });
+
+			Assert::IsNotNull(child.handle());
+			Assert::IsFalse(child.handle() == parent.handle());
+		}
+
+		TEST_METHOD(Bitmap_create)
+		{
+			Bitmap bitmap;
+			bitmap.create(32, 32);
+
+			Assert::IsNotNull(bitmap.handle());
+		}
+		TEST_METHOD(Bitmap_load)
+		{
+			Bitmap bitmap;
+			bitmap.create(32, 32);
+			bitmap.save(m_fileA);
+			bitmap.clear();
+
+			Assert::IsNull(bitmap.handle());
+			bitmap.load(m_fileA);
+			Assert::IsNotNull(bitmap.handle());
+		}
+		TEST_METHOD(Bitmap_save)
+		{
+			Bitmap bitmapA, bitmapB;
+			bitmapA.create(32, 32);
+			bitmapA.save(m_fileA);
+			bitmapB.save(m_fileB);
+
+			Assert::IsTrue(m_fileA.exists());
+			Assert::IsFalse(m_fileB.exists());
+		}
+
+		TEST_METHOD(Bitmap_clear)
+		{
+			Bitmap bitmap;
+			bitmap.create(32, 32);
+			bitmap.clear();
+
+			Assert::IsNull(bitmap.handle());
+		}
+
+		// ...
+
+		TEST_METHOD(Bitmap_lock)
+		{
+			Bitmap bitmapA, bitmapB;
+			bitmapA.create(32, 32);
+			
+			const auto regionA = bitmapA.lock(Bitmap::LockMode::READONLY);
+			const auto regionB = bitmapB.lock(Bitmap::LockMode::READONLY);
+			Assert::IsNotNull(regionA.data());
+			Assert::IsNull(regionB.data());
+		}
+
+	private:
+		const util::File m_fileA = "test_bitmap_file_a.png";
+		const util::File m_fileB = "test_bitmap_file_b.png";
+	};
+}
