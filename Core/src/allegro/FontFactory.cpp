b5c0026739b1140d2e224f93c2d4f822dd064a15
Kristian Mikalsen <Kmikalsen@hotmail.com>
2019-08-10 00:47:02 +0200

    Implemented font loader factory

--- Core/src/allegro/FontFactory.cpp
+++ Core/src/allegro/FontFactory.cpp
@@ -1,7 +1,41 @@
 
 #include "FontFactory.h"
 
+#include <plog/Log.h>
+#include <pugixml/pugixml.hpp>
+
+namespace
+{
+	constexpr const char * NODE_FONT = "font";
+
+	constexpr const char * ATTR_PATH = "path";
+	constexpr const char * ATTR_SIZE = "size";
+	constexpr const char * ATTR_TYPE = "type";
+}
+
 std::unique_ptr<core::Font> core::FontLoader::load(const util::File & file)
 {
-	return nullptr;
+	LOG_INFO << "Loading font " << file.path() << "...";
+	pugi::xml_document doc;
+	doc.load_file(file.path().c_str());
+
+	auto font = std::make_unique<Font>();
+	for (auto node = doc.child(NODE_FONT).first_child(); node; node = node.next_sibling())
+	{
+		const std::string path = node.attribute(ATTR_PATH).as_string();
+		const std::string type = node.attribute(ATTR_TYPE).as_string("regular");
+		const auto size = node.attribute(ATTR_SIZE).as_float();
+
+		if (type == "regular")
+			font->prepare(path, Font::Style::REGULAR, size);
+		else if (type == "bold")
+			font->prepare(path, Font::Style::BOLD, size);
+		else if (type == "bold-italic")
+			font->prepare(path, Font::Style::BOLD_ITALIC, size);
+		else if (type == "italic")
+			font->prepare(path, Font::Style::ITALIC, size);
+		else
+			LOG_WARNING << "Unknown font style " << type;
+	}
+	return font;
 }
--- CoreTest/src/allegro/FontFactoryTest.cpp
+++ CoreTest/src/allegro/FontFactoryTest.cpp
@@ -17,7 +17,13 @@ namespace core::allegro
 		{
 			util::Folder{ "test" }.create();
 			util::File{ "test/font.ttf" }.write(builtin::FONT_DATA, builtin::FONT_SIZE);
-			util::File{ "test/font.xml" }.write(R"( <font size="18" path="test/font.ttf" /> )");
+			util::File{ "test/font-bold.ttf" }.write(builtin::FONT_DATA, builtin::FONT_SIZE);
+			util::File{ "test/font.xml" }.write(R"(
+				<font>
+					<style size="18" path="test/font.ttf" />
+					<style size="18" type="bold" path="test/font-bold.ttf" />
+				</font>
+			)");
 		}
 		TEST_CLASS_CLEANUP(cleanup)
 		{
@@ -27,8 +33,15 @@ namespace core::allegro
 		TEST_METHOD(FontLoader_load)
 		{
 			FontLoader factory;
+			auto font = factory.load("test/font.xml");
 
+			const auto handleA = font->handle(Font::Style::REGULAR);
+			const auto handleB = font->handle(Font::Style::BOLD);
+			const auto handleC = font->handle(Font::Style::ITALIC);
 
+			Assert::IsFalse(handleA == handleB);
+			Assert::IsFalse(handleA == handleC);
+			Assert::IsFalse(handleB == handleC);
 		}
 	};
 }
